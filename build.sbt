9a27975efa29a5fd27ee2bae8e6d0171763cd59b
David Strawn <isomarcte@gmail.com>
2019-02-05 08:20:05 -0700

    squash!

--- build.sbt
+++ build.sbt
@@ -10,6 +10,8 @@ val typelevelG  = "org.typelevel"
 // Artifacts //
 
 val jmhCoreA = "jmh-core"
+val jmhGeneratorAnnProcessA = "jmh-generator-annprocess"
+val jmhGeneratorAsmA = "jmh-generator-asm"
 val catsEffectA = "cats-effect"
 
 // Versions //
@@ -17,16 +19,21 @@ val catsEffectA = "cats-effect"
 val jmhV = "1.21"
 val catsEffectV = "1.2.0"
 
+
 // GAVs //
 
 lazy val jmhCore = openJdkJmhG % jmhCoreA % jmhV
 lazy val catsEffect = typelevelG %% catsEffectA % catsEffectV
+lazy val jmhGeneratorAnnProcess = openJdkJmhG % jmhGeneratorAnnProcessA % jmhV
+lazy val jmhGeneratorAsm = openJdkJmhG % jmhGeneratorAsmA % jmhV
 
 // ThisBuild Scoped Settings //
 
 ThisBuild / organization := "io.isomarcte"
 ThisBuild / scalaVersion := "2.12.8"
 ThisBuild / version      := "1.0.0-SNAPSHOT"
+ThisBuild / scalacOptions += "-target:jvm-1.8"
+ThisBuild / javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
 
 // Enable
 
@@ -53,7 +60,10 @@ lazy val jmh = project.settings(
   name := s"$projectName-jmh",
   libraryDependencies ++= Seq(
     jmhCore
-  )
+  ),
+  mainClass in assembly := Some("org.openjdk.jmh.Main")
 ).dependsOn(
   cats
+).enablePlugins(
+  JmhPlugin
 )
--- jmh/src/main/scala/io/isomarcte/simple/cache/jmh/CacheMissBenchmark.scala
+++ jmh/src/main/scala/io/isomarcte/simple/cache/jmh/CacheMissBenchmark.scala
@@ -3,30 +3,33 @@ package io.isomarcte.simple.cache.jmh
 import cats.effect._
 import io.isomarcte.simple.cache.cats._
 import io.isomarcte.simple.cache.core._
+import java.util.concurrent._
 import org.openjdk.jmh.annotations._
 
+@Warmup(iterations = 10, time = 20)
+@Measurement(iterations = 15, time = 30)
 class CacheMissBenchmark {
   import CacheMissBenchmark._
 
   @Benchmark
-  def catsLockCached(state: CatsLockSynchronizedBenchmarkState): Bar =
-    state.cacheGet(20L).unsafeRunSync
+  def ioFullySynchronizedCached(state: IOSynchronizedBenchmarkState): Bar =
+    state.cacheGet(cacheMissCost).unsafeRunSync
 
   @Benchmark
-  def catsCached(state: CatsSynchronizedBenchmarkState): Bar =
-    state.cacheGet(20L).unsafeRunSync
+  def ioLockCached(state: IOLockSynchronizedBenchmarkState): Bar =
+    state.cacheGet(cacheMissCost).unsafeRunSync
 
   @Benchmark
   def lockCached(state: LockSynchronizedBenchmarkState): Bar =
-    state.cacheGet(20L)
+    state.cacheGet(cacheMissCost)
 
   @Benchmark
   def cached(state: SynchronizedBenchmarkState): Bar =
-    state.cacheGet(20L)
+    state.cacheGet(cacheMissCost)
 
   @Benchmark
   def uncached(state: SynchronizedBenchmarkState): Bar =
-    state.missGet(20L)
+    state.missGet(cacheMissCost)
 }
 
 object CacheMissBenchmark {
@@ -37,6 +40,7 @@ object CacheMissBenchmark {
 
   val fooCount: Int = 10000
   val barCount: Int = 10
+  val cacheMissCost: Long = 10L
 
   val m: Map[Int, String] = {
     val foos: Vector[Int] =
@@ -49,9 +53,20 @@ object CacheMissBenchmark {
     }._1
   }
 
-  abstract class AbstractBenchmarkState[A, B, F[_]] {
+  trait BenchmarkState[F[_]] {
+    def cacheGet(missCost: Long): F[Bar]
+    def missGet(missCost: Long): Bar
+  }
+
+  abstract class AbstractBenchmarkState[F[_]] extends BenchmarkState[F] {
     @volatile private var state: Int = 0
-    protected val cache: Cache.MutableCache[A, B, F[_]]
+    protected def cache: Cache.MutableCache[Foo, Bar, F]
+    protected def pure[A](a: A): F[A]
+    protected def bind[A, B](fa: F[A], f: A => F[B]): F[B]
+
+    protected def fmap[A, B](fa: F[A], f: A => B): F[B] =
+      bind(fa, (a: A) => pure(f(a)))
+
     private def getAndUpdateState: Int = {
       val currentValue: Int = this.state
       if (currentValue == (barCount - 1)) {
@@ -62,179 +77,65 @@ object CacheMissBenchmark {
         currentValue
       }
     }
+
+    private def missGetWithFoo(cost: Long, foo: Foo): Bar = {
+      Thread.sleep(cost)
+      Bar(m(foo.i))
+    }
+
+    private def cacheGetWithFoo(cost: Long, f: Foo): F[Bar] =
+      this.bind(
+        this.cache.get(f),
+        (ob: Option[Bar]) => ob match {
+          case Some(bar) => this.pure(bar)
+          case _ =>
+            val bar: Bar = this.missGetWithFoo(cost, f)
+            this.fmap(this.cache.putIfAbsent(f, bar), Function.const(bar))
+        }
+      )
+
+    private def nextFoo(): Foo =
+      Foo(this.getAndUpdateState)
+
+    override final def cacheGet(cost: Long): F[Bar] =
+      this.cacheGetWithFoo(cost, nextFoo)
+
+    override final def missGet(cost: Long): Bar =
+      this.missGetWithFoo(cost, nextFoo)
   }
 
-  final class SynchronizedBenchmarkState extends AbstractBenchmarkState {
-    private val cache:
+  abstract class IdAbstractBenchmarkState extends AbstractBenchmarkState[Id] {
+    override protected final def pure[A](a: A): Id[A] = a
+    override protected final def bind[A, B](fa: Id[A], f: A => Id[B]): Id[B] = f(fa)
   }
 
   @State(Scope.Benchmark)
-  class SynchronizedBenchmarkState {
-    @volatile
-    private var state: Int = 0
-
-    private val cache: Cache.SimpleMutableCache[Foo, Bar] =
+  class SynchronizedBenchmarkState extends IdAbstractBenchmarkState {
+    override protected final val cache: Cache.MutableCache[Foo, Bar, Id] =
       Cache.concurrentSimpleCacheDefaults
-
-    private def getState: Int = {
-      val currentValue: Int = this.state
-      if (currentValue == (barCount - 1)) {
-        this.state = 0
-        currentValue
-      } else {
-        this.state = currentValue + 1
-        currentValue
-      }
-    }
-
-    private def missGetWithFoo(cost: Long, foo: Foo): Bar = {
-      Thread.sleep(cost)
-      Bar(m(foo.i))
-    }
-
-    private def cacheGetWithFoo(cost: Long, f: Foo): Bar =
-      this.cache.get(f) match {
-        case Some(bar) => bar
-        case _ =>
-          val bar: Bar = this.missGetWithFoo(cost, f)
-          this.cache.putIfAbsent(foo, bar)
-          bar
-      }
-
-    private def foo: Foo =
-      Foo(this.getState)
-
-    def missGet(cost: Long): Bar =
-      this.missGetWithFoo(cost, foo)
-
-    def cacheGet(cost: Long): Bar =
-      this.cacheGetWithFoo(cost, foo)
   }
 
   @State(Scope.Benchmark)
-  class LockSynchronizedBenchmarkState {
-    @volatile
-    private var state: Int = 0
-
-    private val cache: Cache.SimpleMutableCache[Foo, Bar] =
+  class LockSynchronizedBenchmarkState extends IdAbstractBenchmarkState {
+    override protected final val cache: Cache.MutableCache[Foo, Bar, Id] =
       Cache.lockConcurrentSimpleMutableCacheDefaults
+  }
 
-    private def getState: Int = {
-      val currentValue: Int = this.state
-      if (currentValue == (barCount - 1)) {
-        this.state = 0
-        currentValue
-      } else {
-        this.state = currentValue + 1
-        currentValue
-      }
-    }
-
-    private def missGetWithFoo(cost: Long, foo: Foo): Bar = {
-      Thread.sleep(cost)
-      Bar(m(foo.i))
-    }
-
-    private def cacheGetWithFoo(cost: Long, f: Foo): Bar =
-      this.cache.get(f) match {
-        case Some(bar) => bar
-        case _ =>
-          val bar: Bar = this.missGetWithFoo(cost, f)
-          this.cache.putIfAbsent(foo, bar)
-          bar
-      }
-
-    private def foo: Foo =
-      Foo(this.getState)
-
-    def missGet(cost: Long): Bar =
-      this.missGetWithFoo(cost, foo)
-
-    def cacheGet(cost: Long): Bar =
-      this.cacheGetWithFoo(cost, foo)
+  abstract class IOAbstractBenchmarkState extends AbstractBenchmarkState[IO] {
+    override final def pure[A](a: A): IO[A] = IO.pure(a)
+    override final def bind[A, B](fa: IO[A], f: A => IO[B]): IO[B] =
+      fa.flatMap(f)
   }
 
   @State(Scope.Benchmark)
-  class CatsSynchronizedBenchmarkState {
-    @volatile
-    private var state: Int = 0
-
-    private val cache: Cache.MutableCache[Foo, Bar, IO] =
-      CatsCache.concurrentSimpleCatsCacheDefaults[Foo, Bar, IO]
-
-    private def getState: Int = {
-      val currentValue: Int = this.state
-      if (currentValue == (barCount - 1)) {
-        this.state = 0
-        currentValue
-      } else {
-        this.state = currentValue + 1
-        currentValue
-      }
-    }
-
-    private def missGetWithFoo(cost: Long, foo: Foo): Bar = {
-      Thread.sleep(cost)
-      Bar(m(foo.i))
-    }
-
-    private def cacheGetWithFoo(cost: Long, f: Foo): IO[Bar] =
-      this.cache.get(f).flatMap{
-        case Some(bar) => IO.pure(bar)
-        case _ =>
-          val bar: Bar = this.missGetWithFoo(cost, f)
-          this.cache.putIfAbsent(foo, bar).map(Function.const(bar))
-      }
-
-    private def foo: Foo =
-      Foo(this.getState)
-
-    def missGet(cost: Long): Bar =
-      this.missGetWithFoo(cost, foo)
-
-    def cacheGet(cost: Long): IO[Bar] =
-      this.cacheGetWithFoo(cost, foo)
+  class IOSynchronizedBenchmarkState extends IOAbstractBenchmarkState {
+    override protected final val cache: Cache.MutableCache[Foo, Bar, IO] =
+      CatsCache.concurrentSimpleCatsCacheDefaults
   }
 
   @State(Scope.Benchmark)
-  class CatsLockSynchronizedBenchmarkState {
-    @volatile
-    private var state: Int = 0
-
-    private val cache: Cache.MutableCache[Foo, Bar, IO] =
+  class IOLockSynchronizedBenchmarkState extends IOAbstractBenchmarkState {
+    override protected final val cache: Cache.MutableCache[Foo, Bar, IO] =
       CatsCache.lockConcurrentSimpleCatsCacheDefaults
-
-    private def getState: Int = {
-      val currentValue: Int = this.state
-      if (currentValue == (barCount - 1)) {
-        this.state = 0
-        currentValue
-      } else {
-        this.state = currentValue + 1
-        currentValue
-      }
-    }
-
-    private def missGetWithFoo(cost: Long, foo: Foo): Bar = {
-      Thread.sleep(cost)
-      Bar(m(foo.i))
-    }
-
-    private def cacheGetWithFoo(cost: Long, f: Foo): IO[Bar] =
-      this.cache.get(f).flatMap{
-        case Some(bar) => IO.pure(bar)
-        case _ =>
-          val bar: Bar = this.missGetWithFoo(cost, f)
-          this.cache.putIfAbsent(foo, bar).map(Function.const(bar))
-      }
-
-    private def foo: Foo =
-      Foo(this.getState)
-
-    def missGet(cost: Long): Bar =
-      this.missGetWithFoo(cost, foo)
-
-    def cacheGet(cost: Long): IO[Bar] =
-      this.cacheGetWithFoo(cost, foo)
   }
 }
